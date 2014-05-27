906510e2182eeabf3553fab113f7da6844b4a769
guym <guym@gigaspaces.com>
2014-05-27 19:37:12 +0300

    removing shaded plugin references as jclouds does not play nice with it. changing the start.sh command to include classpath dependencies and state the main class.

--- cloudify-widget-pool-manager-website/build/start.sh
+++ cloudify-widget-pool-manager-website/build/start.sh
@@ -3,5 +3,4 @@ source /etc/sysconfig/widget-pool-manager
 VM_ARGS="-Dspring.profiles.active=manager-app"
 VM_ARGS="$VM_ARGS -DwebsiteMeContext=file://$INSTALL_LOCATION/build/me-context.xml"
 
-
-java $VM_ARGS -jar $INSTALL_LOCATION/website-1.0.0.jar
+java $VM_ARGS  -classpath "website-1.0.0.jar:lib/*"  cloudify.widget.website.initializer.EmbeddedJetty
